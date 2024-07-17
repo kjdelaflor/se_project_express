f1b580bd7c3d816452d6ac2bcf42474ae22f9f73
kjdelaflor <kjdelaflor@gmail.com>
2024-07-16 19:12:24 -0700

    need to do users.js

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,5 +1,15 @@
 const ClothingItem = require("../models/clothingItem");
 
+const {
+  OK,
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+  messageBadRequest,
+  messageInternalServerError,
+  messageNotFoundError,
+} = require("../utils/errors");
+
 const addItem = (req, res) => {
   console.log(req);
   console.log(req.body);
@@ -11,16 +21,32 @@ const addItem = (req, res) => {
       console.log(item);
       res.send({ data: item });
     })
-    .catch((e) => {
-      res.status(500).send({ message: "Error from addItem", e });
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: `${messageBadRequest} from createItem` });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: `${messageInternalServerError} from createItem` });
     });
 };
 
 const getItem = (req, res) => {
   ClothingItem.find({})
-    .then((items) => res.status(200).send(items))
-    .catch((e) => {
-      res.status(500).send({ message: "Error from getItems", e });
+    .then((items) => res.status(OK).send(items))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: `${messageBadRequest}from getItems` });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: `${messageInternalServerError} from getItems` });
     });
 };
 
@@ -30,9 +56,17 @@ const updateItem = (req, res) => {
 
   ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
     .orFail()
-    .then((item) => res.status(200).send({ data: item }))
-    .catch((e) => {
-      res.status(500).send({ message: "Error from updateItem", e });
+    .then((item) => res.status(OK).send({ data: item }))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: `${messageBadRequest} from updateItem` });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: `${messageInternalServerError} from updateItem` });
     });
 };
 
@@ -42,28 +76,74 @@ const deleteItem = (req, res) => {
   console.log(itemId);
   ClothingItem.findByIdAndDelete(itemId)
     .orFail()
-    .then((item) => res.status(204).send({}))
-    .catch((e) => {
-      res.status(500).send({ message: "Error from deleteItem", e });
+    .then((item) => res.status(OK).send(item))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError" || err.name === "CastError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: `${messageBadRequest} from deleteItem` });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(NOT_FOUND)
+          .send({ message: `${messageNotFoundError} from deleteItem` });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: `${messageInternalServerError} from deleteItem` });
     });
 };
 
-const likeItem = (req, res) => {
+const likeItem = (req, res) =>
   ClothingItem.findByIdAndUpdate(
-    req.params,
-    itemId,
+    req.params.itemId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
-  );
-
-  console.log(itemId);
-  ClothingItem.findByIdAndDelete(itemId)
+  )
     .orFail()
-    .then((item) => res.status(204).send({}))
-    .catch((e) => {
-      res.status(500).send({ message: "Error from deleteItem", e });
+    .then((item) => res.status(OK).send(item))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: `${messageBadRequest} from likeItem` });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(NOT_FOUND)
+          .send({ message: `${messageNotFoundError} from likeItem` });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: `${messageInternalServerError} from likeItem` });
     });
-};
+
+const dislikeItem = (req, res) =>
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.status(OK).send(item))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: `${messageBadRequest} from dislikeItem` });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(NOT_FOUND)
+          .send({ message: `${messageNotFoundError} from dislikeItem` });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: `${messageInternalServerError} from dislikeItem` });
+    });
 
 module.exports = {
   addItem,
@@ -71,4 +151,6 @@ module.exports = {
   updateItem,
   deleteItem,
   likeItem,
+  ClothingItem,
+  dislikeItem,
 };
